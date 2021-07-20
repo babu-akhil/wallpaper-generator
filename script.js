5805d12367eaa7fd5fab9cfe34feafed3b734b13
Akhil Babu <realakhilbabu@gmail.com>
2021-07-20 17:05:38 +0530

    v1

---
+++ script.js
@@ -0,0 +1,102 @@
+let step;
+let pattern = []
+let n  = 40;
+let cnv;
+function setup() {
+    cnv = createCanvas(800,800);
+    ellipseMode(CORNER)
+}
+
+function draw() {
+    noStroke();
+    background('#bae8e8');
+    step_x = 20;
+    step_y = 20;
+    let x_index = 0;
+
+    let y_index = 0;
+    for(let x =0; x< width; x+= step_x){
+        for(let y=0; y< height; y+= step_y){
+            drawSquare(x,y,step_x, x_index, y_index);
+            y_index++;
+        }
+        y_index =0;
+        x_index++;
+    }
+}
+
+function drawSquare(x,y,width,x_index, y_index){
+    fill(pattern[x_index][y_index])
+    square(x,y,width);
+}
+
+// Init Pattern
+for(let i=0; i<n;i++){
+    pattern[i] = [0,0,0,0,0,0,0,0]
+}
+for(let i=0; i<n; i++){
+    for(let j=0; j<n; j++){
+        pattern[i][j] = '#bae8e8';
+    }
+}
+
+// Initialize to random pattern
+function randomizePattern(){
+    for(let i=0; i<n;i++){
+        pattern[i] = [0,0,0,0,0,0,0,0]
+    }
+    for(let i=0; i<n; i++){
+        for(let j=0; j<n; j++){
+            pattern[i][j] = '#bae8e8';
+        }
+    }
+
+    for(let i=0; i<n/2; i++){
+        for(let j=0; j<n/2; j++){
+            if(Math.random()*(i+j)>20){
+            pattern[i][j] = '#2c698d';
+            }
+        }
+    }
+
+    // Symmetry
+
+    for(let i=n/2;i<n;i++){
+        for(let j=0;j<n/2;j++){
+            pattern[i][j] = pattern[n-1-i][j]
+        }
+    }
+
+    for(let i=n/2;i<n;i++){
+        for(let j=n/2;j<n;j++){
+            pattern[i][j] = pattern[n-1-i][n-1-j]
+        }
+    }
+
+    for(let i=0;i<n/2;i++){
+        for(let j=n/2;j<n;j++){
+            pattern[i][j] = pattern[i][n-1-j]
+        }
+    }
+    setTimeout(() => {document.body.style.background = "url(" + cnv.canvas.toDataURL() + ")";
+                    document.body.style.backgroundPosition = 'center'},
+    1000)
+}
+
+function save(){
+    saveCanvas(cnv, Math.random().toString(36).substring(7), 'png')
+}
+
+
+let saveBtn = document.getElementById('save')
+console.log(saveBtn)
+saveBtn.addEventListener('click', save)
+
+let generateBtn = document.getElementById('generate')
+generateBtn.addEventListener('click', randomizePattern)
+
+randomizePattern();
+
+setTimeout(() => {document.body.style.background = "url(" + cnv.canvas.toDataURL() + ")";
+                    document.body.style.backgroundPosition = 'center'},
+1000)
